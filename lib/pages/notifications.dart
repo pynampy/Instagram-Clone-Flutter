126887ce31fd9436c290f2b16ed7876fe154c851
Naman Jain <yagami.light2433@gmail.com>
2020-08-29 17:00:02 +0530

    initial commit

---
+++ lib/pages/notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/post.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/models/comment.dart';
+import 'package:instagram_clone/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
